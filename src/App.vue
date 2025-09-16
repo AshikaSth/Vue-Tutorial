9b103461f543b8c4aa9f79cbdd25f196a6d1a948
Ashika <shrestha.ashika@cloco.net>
2025-09-16 10:31:14 +0545

    conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
-<div>
-<div> {{greet}} {{ name }}</div>
-<div v-html="channel"></div>
-<div >{{channel}}</div>
-<div v-html="hack"></div>
-<div :id="headingId"><h2>Heading</h2></div>
-<button :disabled="isDisabled">Bind</button>
-<h2 class="underline">Underline</h2>
-<h2 class="underline" :class=" status">Status</h2>
-<h2 :class="isPromoted && 'promoted'" >Promoted Movie</h2>
-<h2 :class="isSoldout? 'sold-out' : 'new'">Soldout? movie</h2>
-<h2 :class="['new', 'promoted']">Newly Promoted movie</h2>
-<h2 :class="[ isPromoted && 'promoted', isSoldout? 'sold-out' : 'new']">Array conditional movie</h2>
-<h2 :class="{
-  promoted: isPromoted,
-  new: !isSoldout,
-  'sold-out': isSoldout,
-}">Object Conditional Movie</h2>
-<h2 :style="{
-  color: highlightColor,
-  'font-size': headerSize + 'px',
-  padding: '20px',
-  }">Inline Style</h2>
-  <h2 :style="headerStyleObject">Style Object</h2>
-  <div :style="[baseStyleObject, successStyleObject]">SuccessStyle</div>
-  <div :style="[baseStyleObject, dangerStyleObject]">Danger Style</div>
+  <h2 v-if="num ===0">The number is zero.</h2>
+  <h2 v-else-if="num < 0">The number is negative.</h2>
+  <h2 v-else-if="num > 0">The number is positive.</h2>
+  <h2 v-else>Not a number.</h2>
+  <template v-if="display">
+    <h2>Ashika</h2>
+    <h2>Codevolution</h2>
+    <h2>Vue</h2>
+  </template>
+  <h2 v-show="showElement">Using v-show</h2>
+    <h2 v-if="showElement">Using v-if</h2>
 
-</div>
 </template>
 
 <script>
@@ -34,38 +18,9 @@ export default {
   name: 'App',
   data() {
     return{
-      greet: "Hello",
-      name: "Batman",
-      channel: "<b>Codevolutiion</b>",
-      hack: `<a href="#" onclick="alert('You have been hacked!')">Win a prize!</a>`,
-      headingId:'heading',
-      isDisabled: true,
-      status: 'success',
-      isPromoted: true,
-      isNew:true,
-      isSoldout: true,
-      highlightColor: 'orange',
-      headerSize: 50,
-      headerStyleObject: {
-        color:'orange',
-        fontSize: '50px',
-        padding: '20px',
-      },
-      baseStyleObject: {
-        fontSize:'50px',
-        padding:'10px',
-      },
-      successStyleObject: {
-        color: 'green',
-        backgroundColor: 'lightGreen',
-        border: '1px solid green',
-        padding: '20px',
-      },
-      dangerStyleObject: {
-         color: 'darkred',
-        backgroundColor: 'red',
-        border: '1px solid darkred',
-      }
+     num:-9,
+     display: true,
+     showElement: false,
     };
   },
 };
