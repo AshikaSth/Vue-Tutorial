2f087855b3f6e5e60e32e20204d6d99944046f61
Ashika <shrestha.ashika@cloco.net>
2025-09-17 09:21:36 +0545

    methods

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
-  <template v-for="name in names" :key="name">
-    <h2 v-if="name ==='Bruce'">{{ name }}</h2>
-    </template>
+<h2>{{ 2+3+5 }}</h2>
+<h2>{{ 5+10+5 }}</h2>
+<h2>Add method- {{ add(5,10,10) }}</h2>
+<h2>Multiply method - {{ multiply(9) }}</h2>
+<h2>Multiply methos - {{ multiply(baseValue) }}</h2>
 </template>
 
 <script>
+
 export default {
   name: 'App',
   data() {
     return{
-      names:['Bruce', 'Clark', 'Diana', 'Barry'],
-    };
+      baseMultiplier: 5,
+      baseValue: 2
+    }
+     
   },
+  methods:{
+    add(a,b,c){
+      return a+b+c
+    },
+    multiply(num){
+      return num * this.baseMultiplier
+    }
+  }
 };
 </script>
 
